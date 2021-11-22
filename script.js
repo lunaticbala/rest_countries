6fceeebc6375b15b8967d80b1baf9209f5f243a5
lunaticbala <87910861+lunaticbala@users.noreply.github.com>
2021-11-22 18:41:10 +0530

    Update script.js

--- script.js
+++ script.js
@@ -26,3 +26,15 @@ xhr.onload=function(){
 
 xhr.open("Get", "https://raw.githubusercontent.com/rvsp/restcountries-json-data/master/res-countries.json")
 xhr.send();
+
+// to compare two JSON have the same properties without order
+
+var obj1 = { name: "Person 1", age:5 };
+var obj2 = { age:5, name: "Person 1" };
+
+    
+    JSON.stringify(obj1)===JSON.stringify(obj2)
+    // false
+    
+    _.isEqual(obj1, obj2)
+    //true
